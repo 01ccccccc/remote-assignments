edbd8aeaafa53439ea4d06ec931ca83873ebc6e6
01ccccccc <cindy92376@gmail.com>
2022-06-16 15:59:16 +0800

    Update assignment-2.js

--- Week-2/Assignment-2/assignment-2.js
+++ Week-2/Assignment-2/assignment-2.js
@@ -1,5 +1,22 @@
 function avg(data) {
-    //code
+    var total;
+    var product = data['products'];
+    
+    // for (var i =0; i < product.length; i++) {
+    //     var prices = product[i]['price'];
+    //     console.log(prices);
+    //     // total += prices;
+    // }
+    
+    total = product.reduce((n, {price}) => n + price, 0);
+    // arr.reduce(callback[accumulator, currentValue, currentIndex, array], initialValue)
+    // arrow function: https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+    // https://stackoverflow.com/questions/23247859/better-way-to-sum-a-property-value-in-an-array
+
+
+    // console.log(total);
+    return total / data['size'];
+
 }
 
 console.log(
@@ -17,7 +34,7 @@ console.log(
             {
                 name: 'Product 3',
                 price: 250,
-            },
+            }
         ],
     })
 ); // should print the average price of all products
