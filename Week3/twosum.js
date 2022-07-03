bbdedf604c0fd8686d39195bacfce583fd7da598
01ccccccc <cindy92376@gmail.com>
2022-07-03 16:17:24 +0800

    Update twosum.js

--- Week3/twosum.js
+++ Week3/twosum.js
@@ -16,6 +16,12 @@ function twoSum(nums, target) {
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([2, 3, 4, 5, 7, 11], 9));
 console.log(twoSum([1, 2, 3, 4, 6, 7], 8));
+
+// https://blog.techbridge.cc/2017/01/21/simple-hash-table-intro/
+// https://levelup.gitconnected.com/how-to-solve-two-sum-in-javascript-d1ebd9dfd3d3
+ 
+// Need to know more about Hash Table
+
 /*
 For example:
 twoSum([2, 7, 11, 15], 9);
